4842907c99f08e5fa007f8e1a71af29824318fec
Filip Gorny <filip.gorny@gmail.com>
2022-07-08 19:40:40 +0200

    Done and undone

--- src/task/controller/task.controller.ts
+++ src/task/controller/task.controller.ts
@@ -1,6 +1,14 @@
 import { FindTasksQuery } from './../query/find-tasks.query';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { ResultTaskDto } from '../dto/result-task.dto';
 import { CreateTaskDto } from './../dto/create-task.dto';
@@ -37,5 +45,39 @@ export class TaskController {
     });
 
     return tasks.map((task) => new ResultTaskDto(task));
+  }
+
+  @Post(':uuid/done')
+  async done(@Param('uuid') uuid: string): Promise<ResultTaskDto> {
+    const task = await this.repository.findOne({
+      uuid: uuid,
+    });
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    task.done = true;
+
+    await this.repository.save(task);
+
+    return new ResultTaskDto(task);
+  }
+
+  @Post(':uuid/undone')
+  async undone(@Param('uuid') uuid: string): Promise<ResultTaskDto> {
+    const task = await this.repository.findOne({
+      uuid: uuid,
+    });
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    task.done = false;
+
+    await this.repository.save(task);
+
+    return new ResultTaskDto(task);
   }
 }
